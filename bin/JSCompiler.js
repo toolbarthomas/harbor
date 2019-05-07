17f5d2cf88cc9d79a55d35c1bb9e4984ac9020e1
Thomas van der velde <contact@toolbarthomas.net>
2019-05-07 13:43:00 +0200

    Filter out empty entries within fileSync

--- bin/JSCompiler.js
+++ bin/JSCompiler.js
@@ -7,22 +7,26 @@ const Logger = require('./common/Logger');
 
 class JSCompiler {
   init(config) {
+    this.config = config;
+
     this.cwd = {
-      main: sync(`${config.THEME_SRC}/main/javascripts/**/*.js`),
-      modules: sync(`${config.THEME_SRC}/modules/*/*/*.js`),
+      main: sync(`${this.config.THEME_SRC}/main/javascripts/**/*.js`),
+      modules: sync(`${this.config.THEME_SRC}/modules/*/*/*.js`),
     };
 
     Object.keys(this.cwd).forEach(directory => {
       const baseDirectory = this.cwd[directory];
 
       if (baseDirectory.length === 0) {
-        Logger.info(`Cannot find any javascript files witin ${join(config.THEME_SRC, directory)}`);
+        Logger.info(
+          `Cannot find any javascript files witin ${join(this.config.THEME_SRC, directory)}`
+        );
       } else {
-        Logger.info(`Transpiling javascripts within: ${join(config.THEME_SRC, directory)}`);
+        Logger.info(`Transpiling javascripts within: ${join(this.config.THEME_SRC, directory)}`);
 
-        this.transpile(baseDirectory, config);
+        this.transpile(baseDirectory, this.config);
 
-        Logger.success(`Done transpiling within ${join(config.THEME_SRC, directory)}`);
+        Logger.success(`Done transpiling within ${join(this.config.THEME_SRC, directory)}`);
       }
     });
   }
@@ -33,7 +37,7 @@ class JSCompiler {
    * @param {Array} baseDirectory Array of javascript files defined from
    * a specific directory.
    */
-  transpile(baseDirectory, config) {
+  transpile(baseDirectory) {
     baseDirectory.forEach(entry => {
       if (!statSync(entry).size) {
         Logger.warning(`Skipping empty file: ${entry}`);
@@ -42,7 +46,7 @@ class JSCompiler {
 
         this.source = readFileSync(entry, 'utf8');
         this.transpiledSource = transform(this.source, { presets: ['@babel/env'] });
-        this.destination = entry.replace(config.THEME_SRC, config.THEME_DIST);
+        this.destination = entry.replace(this.config.THEME_SRC, this.config.THEME_DIST);
 
         /**
          * Create the destination directory before writing the source to
