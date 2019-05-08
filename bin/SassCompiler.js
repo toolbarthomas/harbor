fe84754afed3a7b2e10ac272d87bdf943ce824cf
Thomas van der velde <contact@toolbarthomas.net>
2019-05-08 17:29:21 +0200

    Implements stylelint-scss & drupal-breakpoints

--- bin/SassCompiler.js
+++ bin/SassCompiler.js
@@ -1,13 +1,31 @@
-const { statSync, writeFileSync } = require('fs');
+const breakpoints = require('drupal-breakpoints-scss');
+const { statSync, readFileSync, writeFileSync } = require('fs');
 const { sync } = require('glob');
-const { dirname, join } = require('path');
+const { basename, dirname } = require('path');
 const mkdirp = require('mkdirp');
 const { render } = require('node-sass');
 const globImporter = require('node-sass-glob-importer');
+const postcss = require('postcss');
+const postcssScss = require('postcss-scss');
+const stylelint = require('stylelint');
 
 const Logger = require('./common/Logger');
 
 class SassCompiler {
+  constructor() {
+    /**
+     * Flag to prevent files from being written to the Filesystem if the given
+     * file has any Stylelint errors.
+     */
+    this.stylelintError = false;
+
+    /**
+     * Store commonly used Sass styles to interchange these without writing it
+     * to the Filesystem.
+     */
+    this.commonData = '';
+  }
+
   init(config) {
     return new Promise(cb => {
       this.config = config;
@@ -19,16 +37,22 @@ class SassCompiler {
 
       const baseDirectories = Object.keys(this.cwd);
 
+      this.postcssConfig = {
+        plugins: [stylelint()],
+        extends: ['stylelint-config-recommended-scss', 'stylelint-scss'],
+      };
+
       let queue = 0;
+
+      /**
+       * Prepare Drupal specific utility functions for Sass.
+       */
+      this.prepareDrupalUtilities();
 
       baseDirectories.forEach(async directory => {
         const cwd = this.cwd[directory];
 
-        if (cwd.length === 0) {
-          Logger.warning(
-            `Cannot find any stylesheets witin ${join(this.config.THEME_SRC, directory)}`
-          );
-        } else {
+        if (cwd.length > 0) {
           await this.renderCwd(cwd);
         }
 
@@ -42,6 +66,22 @@ class SassCompiler {
   }
 
   /**
+   * Prepares Drupal specific utility functions for Sass.
+   */
+  prepareDrupalUtilities() {
+    const breakpointConfig = sync(`${process.cwd()}/*.breakpoints.yml`);
+
+    if (breakpointConfig.length > 0 && statSync(breakpointConfig[0]).size) {
+      const source = breakpoints.read(breakpointConfig[0], {
+        vars: true,
+        varsPrefix: 'breakpoint--',
+      });
+
+      this.commonData += source.read().toString();
+    }
+  }
+
+  /**
    * Compiles each entry Sass file within the defined cwd asynchronously.
    *
    * @param {Array} cwd The actual array to process.
@@ -52,10 +92,13 @@ class SassCompiler {
       let queue = 0;
 
       cwd.forEach(async entry => {
-        if (!statSync(entry).size) {
-          Logger.warning(`Skipping empty file: ${entry}`);
-        } else {
-          await this.renderFile(entry);
+        if (String(basename(entry)).indexOf('_') !== 0) {
+          if (!statSync(entry).size) {
+            Logger.warning(`Skipping empty file: ${entry}`);
+          } else {
+            await this.lintFile(entry);
+            await this.renderFile(entry);
+          }
         }
 
         queue += 1;
@@ -72,41 +115,85 @@ class SassCompiler {
   }
 
   /**
+   * Use Stylelint to check for errors within the defined entry file.
+   *
+   * @param {String} entry Path to the source stylesheet to render.
+   */
+  lintFile(entry) {
+    return new Promise(cb => {
+      if (!this.config.THEME_DEVMODE) {
+        return cb();
+      }
+
+      const source = readFileSync(entry);
+
+      return postcss(this.postcssConfig)
+        .process(source, {
+          from: entry,
+          syntax: postcssScss,
+        })
+        .then(result => {
+          this.stylelintError = result.stylelint ? result.stylelint.stylelintError || false : false;
+
+          if (this.stylelintError) {
+            Logger.warning(`Stylelint encountered some problems:`);
+          }
+
+          if (result.messages) {
+            result.messages.forEach(message => {
+              if (message.text) {
+                Logger[message.type || 'info'](
+                  `- ${message.text} | ${entry}:${message.line}:${message.column}`
+                );
+              }
+            });
+          }
+
+          cb();
+        });
+    });
+  }
+
+  /**
    * Compile the given entry Sass file and prepare it for the Filesystem.
    *
    * @param {String} entry Path to the source stylesheet to render.
    */
   renderFile(entry) {
     return new Promise(cb => {
-      const destination = entry.replace(this.config.THEME_SRC, this.config.THEME_DIST);
+      if (this.stylelintError) {
+        Logger.info(`Ignoring file due to Stylelint errors: ${entry}`);
+        cb();
+      } else {
+        const destination = entry.replace(this.config.THEME_SRC, this.config.THEME_DIST);
 
-      Logger.info(`Compiling: ${entry}`);
+        Logger.info(`Compiling: ${entry}`);
 
-      render(
-        {
-          file: entry,
-          outputStyle: 'compact',
-          importer: globImporter(),
-          includePaths: [this.config.THEME_SRC],
-          sourceMap: this.config.THEME_DEVMODE,
-          outFile: destination,
-        },
-        async (error, result) => {
-          if (error) {
-            // Use console.log in favor of message() since nodemon only accepts the log method.
-            // eslint-disable-next-line no-console
-            console.log(
-              `Error at line: ${error.line}, column: ${error.column}.`,
-              error.message,
-              error.file
-            );
+        render(
+          {
+            file: null,
+            data: this.commonData + readFileSync(entry).toString(),
+            outputStyle: 'compact',
+            importer: globImporter(),
+            includePaths: [dirname(entry), this.config.THEME_SRC],
+            sourceMap: this.config.THEME_DEVMODE,
+            outFile: destination,
+          },
+          async (error, result) => {
+            if (error) {
+              Logger.error(
+                `Error at line: ${error.line}, column: ${error.column}. - ${error.file}`,
+                true
+              );
+              Logger.error(error.message, true);
+            } else {
+              await this.writeFile(result, destination);
+            }
+
+            cb();
           }
-
-          await this.writeFile(result, destination);
-
-          cb();
-        }
-      );
+        );
+      }
     });
   }
 
@@ -119,17 +206,17 @@ class SassCompiler {
       mkdirp(dirname(destination), error => {
         if (error) {
           Logger.error(error);
+        } else {
+          // Write the actual css to the filesystem.
+          writeFileSync(destination.replace('.scss', '.css'), result.css.toString());
+
+          // Also write the map file if the development environment is active.
+          if (this.config.THEME_DEVMODE) {
+            writeFileSync(destination.replace('.scss', '.css.map'), result.map.toString());
+          }
+
+          Logger.success(`Done compiling: ${destination.replace('.scss', '.css')}`);
         }
-
-        // Write the actual css to the filesystem.
-        writeFileSync(destination.replace('.scss', '.css'), result.css.toString());
-
-        // Also write the map file if the development environment is active.
-        if (this.config.THEME_DEVMODE) {
-          writeFileSync(destination.replace('.scss', '.css.map'), result.map.toString());
-        }
-
-        Logger.success(`Done compiling: ${destination.replace('.scss', '.css')}`);
 
         cb();
       });
