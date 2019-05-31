f1e1045e305e4fb5c20f2027de921881d6140ad3
Thomas van der velde <contact@toolbarthomas.net>
2019-05-31 13:29:25 +0200

    Implements CLI for Harbor

---
+++ bin/index.js
@@ -0,0 +1,3 @@
+const Harbor = require('./Harbor');
+
+Harbor.init();
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const Harbor = require('./bin/Harbor');
+#! /usr/bin/env node
 
-Harbor.init();
+const { resolve } = require('path');
+const { existsSync, readFileSync } = require('fs');
+const { spawn } = require('child_process');
+
+const packagePath = resolve(process.cwd(), 'package.json');
+
+if (existsSync(packagePath)) {
+  const { scripts } = JSON.parse(readFileSync(packagePath));
+
+  const task = process.argv[2] && scripts[process.argv[2]] ? process.argv[2] : false;
+
+  if (task) {
+    spawn('npm', ['run', task], { shell: true, stdio: 'inherit' });
+  }
+}
