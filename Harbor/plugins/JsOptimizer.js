8661ae046b5704aa745a249cb8e047ae2dd968f8
Thomas van der Velde <contact@toolbarthomas.net>
2021-05-25 16:23:36 +0200

    Use uglify instead of overall minify

--- Harbor/plugins/JsOptimizer.js
+++ Harbor/plugins/JsOptimizer.js
@@ -1,4 +1,5 @@
-const minify = require('minify');
+const { readFile, writeFile } = require('fs');
+const { minify } = require('uglify-js');
 
 const Plugin = require('./Plugin');
 
@@ -21,12 +22,15 @@ class JsOptimizer extends Plugin {
     }
 
     await Promise.all(
-      this.entry.map((name) => new Promise((cb) => this.optimizeCwd(name).then((data) => cb())))
+      this.entry.map((name) => new Promise((cb) => this.optimizeCwd(name).then(cb)))
     );
 
     super.resolve();
   }
 
+  /**
+   * Optimizes the sources from the defined entry glob.
+   */
   async optimizeCwd(cwd) {
     await Promise.all(
       cwd.map(
@@ -34,32 +38,50 @@ class JsOptimizer extends Plugin {
           new Promise((done) => {
             this.Console.log(`Optimizing: ${path}`);
 
-            minify(path, this.config.options || {})
-              .then((data) =>
-                this.optimizeFile(path, data)
-                  .then(done)
-                  .catch((exception) => {
-                    this.Console.error(exception);
-                    super.reject();
-                  })
-              )
-              .catch((exception) => {
+            readFile(path, (exception, data) => {
+              if (exception) {
                 this.Console.error(exception);
-                super.reject();
-              });
+              }
+
+              if (!data) {
+                super.resolve();
+              }
+
+              const result = minify(data.toString(), this.config.options || {});
+
+              if (!result.code || result.error) {
+                if (result.error) {
+                  this.Console.error(`Minify exception encountered for ${path}: ${result.error}`);
+                  this.Console.info(`The original state for ${path} will be used instead.`);
+                }
+              }
+
+              this.optimizeFile(path, result.error ? data.toString() : result.code)
+                .then(done)
+                .catch((exception) => {
+                  this.Console.error(exception);
+
+                  super.reject();
+                });
+            });
           })
       )
     );
   }
 
-  optimizeFile(path, blob) {
+  /**
+   * Writes the optimized file to the filesystem.
+   */
+  write(path, blob) {
     if (!path) {
       this.Console.error(`Unable to optimize script, no path has been defined.`);
+
       return super.reject();
     }
 
     if (!blob) {
       this.Console.error(`Unable to optimize script, no data has been defined for ${path}.`);
+
       return super.reject();
     }
 
